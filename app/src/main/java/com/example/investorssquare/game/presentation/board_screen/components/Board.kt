a2ec50a8cffe7a0849cc742a4a2d0948234a43e5
Lara <larapetkovic021@gmail.com>
2024-09-06 15:32:11 +0200

    feat: New board image design

--- app/src/main/java/com/example/investorssquare/game/presentation/board_screen/components/Board.kt
+++ app/src/main/java/com/example/investorssquare/game/presentation/board_screen/components/Board.kt
@@ -1,5 +1,6 @@
 package com.example.investorssquare.game.presentation.board_screen.components
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
@@ -78,8 +79,7 @@ fun Board(
                         painter = painterResource(context.resources.getIdentifier(board.imageUrl?:"", "drawable", context.packageName)),
                         contentDescription = null,
                         modifier = Modifier
-                            .fillMaxSize()
-                            .graphicsLayer(alpha = 0.5f),
+                            .fillMaxSize(),
                         contentScale = ContentScale.Crop
                     )
                 }
