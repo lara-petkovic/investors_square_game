af5d3fecba0551e4fba0e153ace1be129c703dc9
Lara <larapetkovic021@gmail.com>
2024-09-08 15:29:28 +0200

    feat: Player images instead of circles

--- app/src/main/java/com/example/investorssquare/game/presentation/board_screen/components/PlayerDrawer.kt
+++ app/src/main/java/com/example/investorssquare/game/presentation/board_screen/components/PlayerDrawer.kt
@@ -1,16 +1,19 @@
 package com.example.investorssquare.game.presentation.board_screen.components
 
-import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.lerp
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.times
+import com.example.investorssquare.R
+import com.example.investorssquare.game.domain.model.Player
 import com.example.investorssquare.game.presentation.board_screen.viewModels.PlayerViewModel
 
 @Composable
@@ -19,21 +22,17 @@ fun PlayerDrawer(
     canvasHeight: Dp,
     playersVM: PlayerViewModel,
     fieldIndex: Int,
-    circleSize: Dp = 6.dp,
-    spacing: Dp = 4.dp
+    imageSize: Dp = 16.dp,
+    spacing: Dp = (-5).dp
 ) {
     val players = playersVM.players.collectAsState().value
     val playerPositions = playersVM.playerPositions.collectAsState().value
 
-    // Filter players who are on the specified field
     val playersOnField = players.filterIndexed { playerIndex, _ ->
         playerIndex < playerPositions.size && playerPositions[playerIndex] == fieldIndex
     }
 
-    Canvas(modifier = Modifier.size(canvasWidth, canvasHeight)) {
-        val widthPx = canvasWidth.toPx()
-        val heightPx = canvasHeight.toPx()
-
+    Box(modifier = Modifier.size(canvasWidth, canvasHeight)) {
         val rows = when (playersOnField.size) {
             6, 5 -> 3
             4, 3, 2 -> 2
@@ -44,32 +43,45 @@ fun PlayerDrawer(
             else -> 1
         }
 
-        val totalWidthPx = (circleSize + spacing) * cols - spacing
-        val totalHeightPx = (circleSize + spacing) * rows - spacing
-        val startX = (widthPx - totalWidthPx.toPx()) / 2
-        val startY = (heightPx - totalHeightPx.toPx()) / 2
+        val totalWidthPx = (imageSize + spacing) * cols - spacing
+        val totalHeightPx = (imageSize + spacing) * rows - spacing
 
-        // Draw each player who is on the current field
+        val startX = (canvasWidth - totalWidthPx) / 2
+        val startY = (canvasHeight - totalHeightPx) / 2
+
         playersOnField.forEachIndexed { idx, player ->
             val row = idx / cols
             val col = idx % cols
-            val x = startX + (col * (circleSize + spacing)).toPx()
-            val y = startY + (row * (circleSize + spacing)).toPx()
-            val radius = circleSize.toPx() / 2
+            val x = startX + (col * (imageSize + spacing))
+            val y = startY + (row * (imageSize + spacing))
 
-            // Draw the border circle
-            drawCircle(
-                color = lerp(player.color, Color.Black, 0.2f),
-                radius = radius + 1.dp.toPx(),
-                center = Offset(x + radius, y + radius)
-            )
+            val playerImageRes = imageByColor(player)
 
-            // Draw the player circle
-            drawCircle(
-                color = player.color,
-                radius = radius,
-                center = Offset(x + radius, y + radius)
+            Image(
+                painter = painterResource(id = playerImageRes),
+                contentDescription = "Player Icon",
+                modifier = Modifier
+                    .size(imageSize)
+                    .offset(x, y)
             )
         }
     }
+}
+
+@Composable
+private fun imageByColor(player: Player): Int {
+    val playerImageRes = when (player.color) {
+        Color.Magenta -> R.drawable.player_purple    //*
+        Color.Cyan -> R.drawable.player_cyan
+        Color.Green -> R.drawable.player_green
+        Color.Blue -> R.drawable.player_blue
+        Color.Gray -> R.drawable.player_gray
+        Color.DarkGray -> R.drawable.player_brown    //*
+        Color.White -> R.drawable.player_orange      //*
+        Color.Red -> R.drawable.player_red
+        Color.Yellow -> R.drawable.player_yellow
+        Color.Black -> R.drawable.player_black
+        else -> R.drawable.player_black
+    }
+    return playerImageRes
 }
