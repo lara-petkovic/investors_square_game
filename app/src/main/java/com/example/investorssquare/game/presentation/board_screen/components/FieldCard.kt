ed6367234e4dd4199456d1b5830ac5c0888096e8
Lara <larapetkovic021@gmail.com>
2024-10-09 15:38:28 +0200

    feat: Houses drawer implementation.
    Note: Left to handle the drawing of the text when there is 6+ houses

--- app/src/main/java/com/example/investorssquare/game/presentation/board_screen/components/FieldCard.kt
+++ app/src/main/java/com/example/investorssquare/game/presentation/board_screen/components/FieldCard.kt
@@ -1,16 +1,23 @@
 package com.example.investorssquare.game.presentation.board_screen.components
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -18,6 +25,8 @@ import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.example.investorssquare.game.domain.model.Field
@@ -53,6 +62,7 @@ fun FieldCard(
     }
 }
 
+@SuppressLint("StateFlowValueCalledInComposition")
 @Composable
 private fun FieldCardContent(
     field: Field,
@@ -63,6 +73,7 @@ private fun FieldCardContent(
 
     Box(modifier = Modifier.fillMaxSize()) {
         OwnershipIndicator(isOwned = isFieldOwned)
+
         if (field.type == FieldType.PROPERTY) {
             PropertyStrap(
                 property = field as? Property,
@@ -100,7 +111,17 @@ private fun PropertyStrap(
                     strokeWidth = strokeWidth
                 )
             }
-    )
+    ) {
+        property?.let {
+            val estateVM = Game.getEstateByFieldIndex(it.index)
+            val numberOfHouses = estateVM?.numberOfBuildings?.collectAsState()?.value ?: 0
+
+            DrawHouses(
+                numberOfHouses = numberOfHouses,
+                modifier = Modifier.align(Alignment.Center)
+            )
+        }
+    }
 }
 
 @Composable
@@ -111,4 +132,81 @@ private fun OwnershipIndicator(isOwned: Boolean) {
             .alpha(if (isOwned) 0.5f else 0.0f)
             .background(Color(0xFFDAF7DB))
     )
+}
+
+
+@Composable
+fun DrawHouses(
+    numberOfHouses: Int,
+    modifier: Modifier = Modifier
+) {
+    Box(modifier = modifier) {
+        when (numberOfHouses) {
+            in 0..3 -> {
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceEvenly
+                ) {
+                    repeat(numberOfHouses) {
+                        HouseIcon()
+                    }
+                }
+            }
+            in 4..6 -> {
+                var i = 0
+                if (numberOfHouses % 2 != 0) {
+                    i = 1
+                }
+
+                Column(
+                    modifier = Modifier.fillMaxWidth(),
+                    verticalArrangement = Arrangement.SpaceEvenly
+                ) {
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.SpaceEvenly
+                    ) {
+                        repeat(numberOfHouses / 2) { HouseIcon() }
+                    }
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.SpaceEvenly
+                    ) {
+                        repeat(numberOfHouses / 2 + i) { HouseIcon() }
+                    }
+                }
+            }
+            else -> {
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Center
+                ) {
+                    HouseIcon()
+                    Text(text = "x$numberOfHouses")
+                }
+            }
+        }
+    }
+}
+
+
+@SuppressLint("StateFlowValueCalledInComposition")
+@Composable
+fun HouseIcon(
+    modifier: Modifier = Modifier
+) {
+    val context = LocalContext.current
+
+        val resourceId = context.resources.getIdentifier(
+            Game.board.value?.houseImageUrl, "drawable", context.packageName
+        )
+
+        if (resourceId != 0) {
+            Image(
+                painter = painterResource(id = resourceId),
+                contentDescription = null,
+                modifier = modifier.size(8.dp)
+            )
+        }
 }
